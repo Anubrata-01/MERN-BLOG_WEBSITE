0277d26dfa8a44853c59b4835ee834c06f270375
Anubrata-01 <anubratachanda36@gmail.com>
2025-01-24 10:37:04 +0530

    create schema for post and user

--- Server/Models/Users.js
+++ Server/Models/Users.js
@@ -5,6 +5,11 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   isAdmin: { type: Boolean, default: false },
-});
+  
+
+},
+{ timestamps: true }
+
+);
 const UserSchema=mongoose.model('User',userSchema);
 export default UserSchema;
